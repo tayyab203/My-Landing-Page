f8e37fa78e15ac9f3f5aa1c2e26360f021e1192e
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-27 01:02:01 -0700

    commit 10

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function closeNavbar(e) {
     document.body.removeEventListener('click', closeNavbar);
   }
 }
+
+// Toggle nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+    document.body.addEventListener('click', closeNavbar);
+  });
+  
+  // Show modal
+  open.addEventListener('click', () => modal.classList.add('show-modal'));
+  
+  // Hide modal
+  close.addEventListener('click', () => modal.classList.remove('show-modal'));
+  
+  // Hide modal on outside click
+  window.addEventListener('click', e =>
+    e.target == modal ? modal.classList.remove('show-modal') : false
+  );
